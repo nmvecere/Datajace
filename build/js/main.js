8ea7db77d3a81ffd122be434810b0e549f7bebc4
Nicole Vecere <nmvecere@gmail.com>
2014-02-25 00:00:57 -0500

    Updated color scheme. Added and linked to settings page. Implemented landscape warning.

--- build/js/main.js
+++ build/js/main.js
@@ -1,3 +1,15 @@
 function pageSetup() {
-	console.log("Hello!");
+	var a=document.getElementsByTagName("a");
+	for(var i=0;i<a.length;i++)
+	{
+		 a[i].onclick=function()
+		 {
+			  window.location=this.getAttribute("href");
+			  return false
+		 }
+	}
+}
+
+function changePage(whichPage) {
+	location.href = whichPage;
 }
