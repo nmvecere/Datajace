f839f6734ae97fc806f862e59ea664480523ec9b
Nicole Vecere <nmvecere@gmail.com>
2014-02-25 13:35:49 -0500

    Removed navigation from landscape mode. Removed unnecessary links from supplementary HTML pages. Simplified Javascript.

--- build/js/main.js
+++ build/js/main.js
@@ -1,15 +1,7 @@
 function pageSetup() {
-	var a=document.getElementsByTagName("a");
-	for(var i=0;i<a.length;i++)
-	{
-		 a[i].onclick=function()
-		 {
-			  window.location=this.getAttribute("href");
-			  return false
-		 }
-	}
+    console.log('Page setup.');
 }
 
-function changePage(whichPage) {
-	location.href = whichPage;
+function changePage(page) {
+	location.href = page;
 }
