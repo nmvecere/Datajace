e32d3148df6b8ff53e7f8261dc4c1901d7a4bb6f
Nicole Vecere <nmvecere@gmail.com>
2014-02-25 15:08:02 -0500

    Changed 'Player 1' and 'Player 2' to text fields. Implemented switches on 'Settings' page.

--- build/js/main.js
+++ build/js/main.js
@@ -5,3 +5,10 @@ function pageSetup() {
 function changePage(page) {
 	location.href = page;
 }
+
+// Switchery test
+var elems = Array.prototype.slice.call(document.querySelectorAll('.js-switch'));
+
+elems.forEach(function(html) {
+  var switchery = new Switchery(html, {color: '#41b7f1'});
+});
