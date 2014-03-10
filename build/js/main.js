da39a60e110a225276a612ff9bc040b4a7b3ae23
Nicole Vecere <nmvecere@gmail.com>
2014-03-09 22:09:53 -0400

    Implemented highlighting and edited HTML/CSS structure. Implemented crude incrementation.

--- build/js/main.js
+++ build/js/main.js
@@ -1,5 +1,5 @@
 function pageSetup() {
-    console.log('Page setup.');
+    console.log("Page setup.")
 }
 
 function changePage(page) {
@@ -7,8 +7,43 @@ function changePage(page) {
 }
 
 // Switchery test
-var elems = Array.prototype.slice.call(document.querySelectorAll('.js-switch'));
+var elems = Array.prototype.slice.call(document.querySelectorAll(".js-switch"));
 
 elems.forEach(function(html) {
-  var switchery = new Switchery(html, {color: '#41b7f1'});
+  var switchery = new Switchery(html, {color: "#41b7f1"});
+});
+
+//$(document).ready(function(){
+//    var playerOneLife = 0;
+//    var playerTwoLife = 0;
+//    
+//    
+//}
+
+// BUTTON TEST
+
+// VARIABLE DECLARATIONS
+
+var $allCounters = $('.counter');
+
+var playerOneLife = 20;
+var playerOneGeneral = 0;
+
+// COUNTER ACTIVATION
+$allCounters.click(function(){
+    $allCounters.removeClass('active');
+    $(this).addClass('active');
+});
+
+// PLAYER ONE COUNTER CONTROLS
+$("#playerOneAdd").click(function(){
+    if ($("#playerOneLife").hasClass("active")) {
+        playerOneLife++;
+        $(".playerOneLifeNum").html(playerOneLife);
+    }
+    else if ($("#playerOneGeneral").hasClass("active")) {
+        playerOneGeneral++;
+        $(".playerOneGeneralNum").html(playerOneGeneral);
+    }
+    // else if...
 });
