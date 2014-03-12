ff6086d5565bf30a54f368dd0a703e6321bc4deb
Nicole Vecere <nmvecere@gmail.com>
2014-03-12 11:43:30 -0400

    Implemented working reset button.

--- build/js/main.js
+++ build/js/main.js
@@ -1,146 +1,184 @@
 //CHECK IF LOCALSTORAGE VARIABLES ARE SET
 //IF NOT, SET THEM
 //IF SO, RETRIEVE THEM
-
 function pageSetup() {
-    if (typeof localStorage !== 'undefined' && localStorage !== null) {
+    if (typeof localStorage.getItem('playerTwoLife') !== 'undefined' && localStorage !== null) {
         var playerOneLife = localStorage.getItem('playerOneLife');
         $('.playerOneLifeNum').html(playerOneLife);
-        
+
         var playerOneGeneral = localStorage.getItem('playerOneGeneral');
         $('.playerOneGeneralNum').html(playerOneGeneral);
-        
+
         var playerOnePoison = localStorage.getItem('playerOnePoison');
         $('.playerOnePoisonNum').html(playerOnePoison);
+
+        var playerTwoLife = localStorage.getItem('playerTwoLife');
+        $('.playerTwoLifeNum').html(playerTwoLife);
+
+        var playerTwoGeneral = localStorage.getItem('playerTwoGeneral');
+        $('.playerTwoGeneralNum').html(playerTwoGeneral);
+
+        var playerTwoPoison = localStorage.getItem('playerTwoPoison');
+        $('.playerTwoPoisonNum').html(playerTwoPoison);
     } else {
         localStorage.setItem('playerOneLife', 20);
         localStorage.setItem('playerOneGeneral', 0);
         localStorage.setItem('playerOnePoison', 0);
+        localStorage.setItem('playerTwoLife', 20);
+        localStorage.setItem('playerTwoGeneral', 0);
+        localStorage.setItem('playerTwoPoison', 0);
     }
 }
 
 // Define function to change page.
 function changePage(page) {
-	location.href = page;
+    location.href = page;
 }
 
 $(document).ready(function () {
 
-// Test Switchery.
-var elems = Array.prototype.slice.call(document.querySelectorAll('.js-switch'));
+    // Test Switchery.
+    var elems = Array.prototype.slice.call(document.querySelectorAll('.js-switch'));
 
-elems.forEach(function(html) {
-  var switchery = new Switchery(html, {color: '#41b7f1'});
-});
-    
-// SETTINGS TEST
-    
-var generalSetting = document.querySelector('.general-setting');
-var poisonSetting = document.querySelector('.poison-setting');
-var mirrorSetting = document.querySelector('.mirror-setting');
+    elems.forEach(function (html) {
+        var switchery = new Switchery(html, {
+            color: '#41b7f1'
+        });
+    });
 
-//generalSetting.onchange = function() {
-//    localStorage.setItem('generalSetting', generalSetting.checked);
-//    console.log(localStorage.getItem('generalSetting'));
-//};
-//    
-//poisonSetting.onchange = function() {
-//    localStorage.setItem('poisonSetting', poisonSetting.checked);
-//    console.log(localStorage.getItem('poisonSetting'));
-//};
-//    
-//mirrorSetting.onchange = function() {
-//    localStorage.setItem('mirrorSetting', mirrorSetting.checked);
-//    console.log(localStorage.getItem('mirrorSetting'));
-//};
-//    
-//if (localStorage.getItem('mirrorSetting') == 'true') {
-//    console.log('TEST');
-//};
+    // SETTINGS TEST
 
-// Define variables.
-var $allCounters = $('.counter');
-var playerTwoLife = 20;
-var playerTwoGeneral = 0;
-var playerTwoPoison = 0;
-    
+    var generalSetting = document.querySelector('.general-setting');
+    var poisonSetting = document.querySelector('.poison-setting');
+    var mirrorSetting = document.querySelector('.mirror-setting');
 
-// Select counter.
-$allCounters.click(function(){
-    $allCounters.removeClass('active');
-    $(this).addClass('active');
-});
+    //generalSetting.onchange = function() {
+    //    localStorage.setItem('generalSetting', generalSetting.checked);
+    //    console.log(localStorage.getItem('generalSetting'));
+    //};
+    //    
+    //poisonSetting.onchange = function() {
+    //    localStorage.setItem('poisonSetting', poisonSetting.checked);
+    //    console.log(localStorage.getItem('poisonSetting'));
+    //};
+    //    
+    //mirrorSetting.onchange = function() {
+    //    localStorage.setItem('mirrorSetting', mirrorSetting.checked);
+    //    console.log(localStorage.getItem('mirrorSetting'));
+    //};
+    //    
+    //if (localStorage.getItem('mirrorSetting') == 'true') {
+    //    console.log('TEST');
+    //};
 
-// Set up player one counter controls.
-$('#playerOneAdd').click(function(){
-    if ($('#playerOneLife').hasClass('active')) {
-        var playerOneLife = localStorage.getItem('playerOneLife');
-        playerOneLife++;
-        $('.playerOneLifeNum').html(playerOneLife);
-        localStorage.setItem('playerOneLife', playerOneLife);
-    }
-    else if ($('#playerOneGeneral').hasClass('active')) {
-        var playerOneGeneral = localStorage.getItem('playerOneGeneral');
-        playerOneGeneral++;
-        $('.playerOneGeneralNum').html(playerOneGeneral);
-        localStorage.setItem('playerOneGeneral', playerOneGeneral);
-    }
-    else if ($('#playerOnePoison').hasClass('active')) {
-        var playerOnePoison = localStorage.getItem('playerOnePoison');
-        playerOnePoison++;
-        $('.playerOnePoisonNum').html(playerOnePoison);
-        localStorage.setItem('playerOnePoison', playerOnePoison);
-    }
-});
+    // Define variables.
+    var $allCounters = $('.counter');
 
-$('#playerOneSub').click(function(){
-    if ($('#playerOneLife').hasClass('active')) {
-        playerOneLife--;
-        $('.playerOneLifeNum').html(playerOneLife);
-    }
-    else if ($('#playerOneGeneral').hasClass('active')) {
-        playerOneGeneral--;
-        $('.playerOneGeneralNum').html(playerOneGeneral);
-    }
-    else if ($('#playerOnePoison').hasClass('active')) {
-        playerOnePoison--;
-        $('.playerOnePoisonNum').html(playerOnePoison);
-    }
-});
+    // Select counter.
+    $allCounters.click(function () {
+        $allCounters.removeClass('active');
+        $(this).addClass('active');
+    });
 
-// Set up player two counter controls.
-$('#playerTwoAdd').click(function(){
-    if ($('#playerTwoLife').hasClass('active')) {
-        playerTwoLife++;
-        $('.playerTwoLifeNum').html(playerTwoLife);
-    }
-    else if ($('#playerTwoGeneral').hasClass('active')) {
-        playerTwoGeneral++;
-        $('.playerTwoGeneralNum').html(playerTwoGeneral);
-    }
-    else if ($('#playerTwoPoison').hasClass('active')) {
-        playerTwoPoison++;
-        $('.playerTwoPoisonNum').html(playerTwoPoison);
-    }
-});
+    // Set up player one counter controls.
+    $('#playerOneAdd').click(function () {
+        if ($('#playerOneLife').hasClass('active')) {
+            var playerOneLife = localStorage.getItem('playerOneLife');
+            playerOneLife++;
+            $('.playerOneLifeNum').html(playerOneLife);
+            localStorage.setItem('playerOneLife', playerOneLife);
+        } else if ($('#playerOneGeneral').hasClass('active')) {
+            var playerOneGeneral = localStorage.getItem('playerOneGeneral');
+            playerOneGeneral++;
+            $('.playerOneGeneralNum').html(playerOneGeneral);
+            localStorage.setItem('playerOneGeneral', playerOneGeneral);
+        } else if ($('#playerOnePoison').hasClass('active')) {
+            var playerOnePoison = localStorage.getItem('playerOnePoison');
+            playerOnePoison++;
+            $('.playerOnePoisonNum').html(playerOnePoison);
+            localStorage.setItem('playerOnePoison', playerOnePoison);
+        }
+    });
 
-$('#playerTwoSub').click(function(){
-    if ($('#playerTwoLife').hasClass('active')) {
-        playerTwoLife--;
-        $('.playerTwoLifeNum').html(playerTwoLife);
-    }
-    else if ($('#playerTwoGeneral').hasClass('active')) {
-        playerTwoGeneral--;
-        $('.playerTwoGeneralNum').html(playerTwoGeneral);
-    }
-    else if ($('#playerTwoPoison').hasClass('active')) {
-        playerTwoPoison--;
-        $('.playerTwoPoisonNum').html(playerTwoPoison);
-    }
-});
+    $('#playerOneSub').click(function () {
+        if ($('#playerOneLife').hasClass('active')) {
+            var playerOneLife = localStorage.getItem('playerOneLife');
+            playerOneLife--;
+            $('.playerOneLifeNum').html(playerOneLife);
+            localStorage.setItem('playerOneLife', playerOneLife);
+        } else if ($('#playerOneGeneral').hasClass('active')) {
+            var playerOneGeneral = localStorage.getItem('playerOneGeneral');
+            playerOneGeneral--;
+            $('.playerOneGeneralNum').html(playerOneGeneral);
+            localStorage.setItem('playerOneGeneral', playerOneGeneral);
+        } else if ($('#playerOnePoison').hasClass('active')) {
+            var playerOnePoison = localStorage.getItem('playerOnePoison');
+            playerOnePoison--;
+            $('.playerOnePoisonNum').html(playerOnePoison);
+            localStorage.setItem('playerOnePoison', playerOnePoison);
+        }
+    });
 
-// Allow user to reset counters.
-$('#resetButton').click(function(){
-    localStorage.setItem('playerOneLife', 20);
-})
+    // Set up player two counter controls.
+    $('#playerTwoAdd').click(function () {
+        if ($('#playerTwoLife').hasClass('active')) {
+            var playerTwoLife = localStorage.getItem('playerTwoLife');
+            playerTwoLife++;
+            $('.playerTwoLifeNum').html(playerTwoLife);
+            localStorage.setItem('playerTwoLife', playerTwoLife);
+        } else if ($('#playerTwoGeneral').hasClass('active')) {
+            var playerTwoGeneral = localStorage.getItem('playerTwoGeneral');
+            playerTwoGeneral++;
+            $('.playerTwoGeneralNum').html(playerTwoGeneral);
+            localStorage.setItem('playerTwoGeneral', playerTwoGeneral);
+        } else if ($('#playerTwoPoison').hasClass('active')) {
+            var playerTwoPoison = localStorage.getItem('playerTwoPoison');
+            playerTwoPoison++;
+            $('.playerTwoPoisonNum').html(playerTwoPoison);
+            localStorage.setItem('playerTwoPoison', playerTwoPoison);
+        }
+    });
+
+    $('#playerTwoSub').click(function () {
+        if ($('#playerTwoLife').hasClass('active')) {
+            var playerTwoLife = localStorage.getItem('playerTwoLife');
+            playerTwoLife--;
+            $('.playerTwoLifeNum').html(playerTwoLife);
+            localStorage.setItem('playerTwoLife', playerTwoLife);
+        } else if ($('#playerTwoGeneral').hasClass('active')) {
+            var playerTwoGeneral = localStorage.getItem('playerTwoGeneral');
+            playerTwoGeneral--;
+            $('.playerTwoGeneralNum').html(playerTwoGeneral);
+            localStorage.setItem('playerTwoGeneral', playerTwoGeneral);
+        } else if ($('#playerTwoPoison').hasClass('active')) {
+            var playerTwoPoison = localStorage.getItem('playerTwoPoison');
+            playerTwoPoison--;
+            $('.playerTwoPoisonNum').html(playerTwoPoison);
+            localStorage.setItem('playerTwoPoison', playerTwoPoison);
+        }
+    });
+
+    // Allow user to reset counters.
+    $('#resetButton').click(function () {
+        var lifeTotal = 20;
+        var zero = 0;
+
+        $('.playerOneLifeNum').html(lifeTotal);
+        localStorage.setItem('playerOneLife', lifeTotal);
+
+        $('.playerOneGeneralNum').html(zero);
+        localStorage.setItem('playerOneGeneral', zero);
+
+        $('.playerOnePoisonNum').html(zero);
+        localStorage.setItem('playerOnePoison', zero);
+
+        $('.playerTwoLifeNum').html(lifeTotal);
+        localStorage.setItem('playerTwoLife', lifeTotal);
+
+        $('.playerTwoGeneralNum').html(zero);
+        localStorage.setItem('playerTwoGeneral', zero);
+
+        $('.playerTwoPoisonNum').html(zero);
+        localStorage.setItem('playerTwoPoison', zero);
+    });
 });
